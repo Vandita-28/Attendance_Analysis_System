8ed10f8d6567b4d053ec8a4450a19234f15a5916
sayali <sayalijoshi792@gmail.com>
2025-04-11 12:36:33 +0530

    Import datasets

---
+++ generateReport.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+cabin = pd.read_csv("Cabin_Info.csv")
+schedule = pd.read_csv("Class_Schedule.csv")
+attendance = pd.read_csv("Employee_Attendance_Data.csv")
+leave = pd.read_csv("Leave_Info.csv")
+slots = pd.read_csv("Slots.csv")
+
